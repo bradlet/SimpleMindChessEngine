14f34cfdfb0b2ec4146a3f2547ba4fba3faa76fe
Bradley Thompson <bradlet2@pdx.edu>
2020-11-30 18:44:36 -0800

    Move script to main code block so filepath can be re-used

--- DatasetBuilder.py
+++ DatasetBuilder.py
@@ -13,34 +13,34 @@ from ChessHelper import ChessHelper as ch
 
 DATASET_FILE_PATH = "./data/chess.data"
 
-if len(sys.argv) < 2:
-    print("Usage: missing pgn file.")
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: missing pgn file.")
 
-pgn = open(sys.argv[1])
-chess_game = chess.pgn.read_game(pgn)
+    pgn = open(sys.argv[1])
+    chess_game = chess.pgn.read_game(pgn)
 
-result = chess_game.headers["Result"].split('-')
-winner = chess.WHITE if result[0] == '1' else chess.BLACK
+    result = chess_game.headers["Result"].split('-')
+    winner = chess.WHITE if result[0] == '1' else chess.BLACK
 
-# Need to initialize np array with empty vector, needs chopping off after game processing
-board_state_history = np.zeros((768+1,))  # 786 input units, 1 data classification label
+    # Need to initialize np array with empty vector, needs chopping off after game processing
+    board_state_history = np.zeros((768+1,))  # 786 input units, 1 data classification label
 
-board = chess.Board()
-for move in chess_game.mainline_moves():
-    board.push(move)
-    data_vector = np.reshape(ch.bitmap_representation(board), (768,))
-    data_label = 1 if board.turn is winner else 0
-    data_line = np.append(data_vector, data_label)
-    board_state_history = np.vstack((board_state_history, data_line))
+    board = chess.Board()
+    for move in chess_game.mainline_moves():
+        board.push(move)
+        data_vector = np.reshape(ch.bitmap_representation(board), (768,))
+        data_label = 1 if board.turn is winner else 0
+        data_line = np.append(data_vector, data_label)
+        board_state_history = np.vstack((board_state_history, data_line))
 
-board_state_history = board_state_history[1:]  # Chop off that empty initialized vector
+    board_state_history = board_state_history[1:]  # Chop off that empty initialized vector
 
-# Getting rid of unwanted characters is annoyingly particular with numpy arrays...
-data = map(str, board_state_history)  # Make each row, corresponding to a given board state, a string.
-data = map(lambda x: x.replace('\n', ''), data)  #
-data = '\n'.join(data).replace('[', '').replace(']', '')
+    # Getting rid of unwanted characters is annoyingly particular with numpy arrays...
+    data = map(str, board_state_history)  # Make each row, corresponding to a given board state, a string.
+    data = map(lambda x: x.replace('\n', ''), data)  #
+    data = '\n'.join(data).replace('[', '').replace(']', '')
 
-dataset_file = open(DATASET_FILE_PATH, 'a')
-dataset_file.writelines(data)
-dataset_file.close()
-
+    dataset_file = open(DATASET_FILE_PATH, 'a')
+    dataset_file.writelines(data)
+    dataset_file.close()
